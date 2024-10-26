1af2389013b032d02dc4a6ce9c21b9ce0c8c85dc
Tiago Alonzo <tiaguito1073@gmail.com>
2024-10-26 17:00:06 -0300

    en proceso

--- inicio/urls.py
+++ inicio/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path 
-from inicio.views import inicio, crear_la_propiedad  
+from inicio.views import inicio, crear_la_propiedad 
 
 urlpatterns = [
-    path ("", inicio),
-    path ("crear-la-propiedad/<int:metros_cuadrados>/<str:ubicacion>/<str:precio>", crear_la_propiedad)
-]
+    path ("", inicio, name= 'inicio' ),
+    path ("crear-la-propiedad/<int:metros_cuadrados>/<str:ubicacion>/<str:precio>", crear_la_propiedad, name= 'crear_la_propiedad'),
+] 
